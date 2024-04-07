10006c0c8dcecba9b4f9f1cc5c7de64bbc3790a4
DHANDI ADAM <148371521+DhandiAdam@users.noreply.github.com>
2024-04-07 23:12:02 +0700

    TUGAS 4 01 & 02

--- Tugas4-Golang-Dhandi-4522210147/Tugas4-4522210147-DhandiAdam.go
+++ Tugas4-Golang-Dhandi-4522210147/Tugas4-4522210147-DhandiAdam.go
@@ -1,23 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Perkalian Factorial 7 ")
-	fmt.Println(fact(7))
-
+	result := fact(7)
+	fmt.Printf("hasil dari perkalian factorial 7 adalah = %d\n", result)
 }
 
 func fact(n int) int {
 	if n == 0 || n == 1 {
 		fmt.Printf("%d x 1\n ", n)
 		return 1
-
 	}
 	fmt.Printf("%d X  ", n)
 	result := n * fact(n-1)
 	fmt.Printf("= %d\n", result)
+
 	return result
 }
